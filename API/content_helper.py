5b31b32aa48859215f64c55b4a65a477d3576d66
moonlightlane <zw16@rice.edu>
2017-06-06 11:04:24 -0500

    added merge term and merge content by chapter functionality

--- API/content_helper.py
+++ API/content_helper.py
@@ -3,13 +3,16 @@ import pattern.en
 import spacy
 import itertools
 
-
+'''
+########################################################################################################################
+'''
 def augment_term(df):
 
     df = lowercase_term(df)
     df = pluralize_term(df)
 
     return df
+
 
 def lowercase_term(df):
     """
@@ -104,10 +107,113 @@ def pluralize_term(df):
                         if terms[i] != singular_term and terms[i] != plural_term:
                             new_term.append(components[0][k]+' '+terms[i].split(' ')[-1])
             else:
-                new_term = [singular_term, plural_term]
+                if terms[i] != singular_term and terms[i] != plural_term:
+                    new_term = [singular_term, plural_term, terms[i]]
+                else:
+                    new_term = [singular_term, plural_term]
 
             new_terms.append(new_term)
 
         df['terms'].replace(terms_arrays[idx], str(new_terms), inplace=True)
 
     return df
+
+
+
+'''
+########################################################################################################################
+'''
+def term_to_JSON(df):
+    '''
+
+    :param df: dataframe of the textbook content
+    :param term: the term you want to query
+    :return: a JASON representation contain the chapter of the term, the module of the term, the name of the module,
+             title of the chapter, all the sentences it contains
+    '''
+
+    # collect all sentences to a single string
+    content = merge_contents(df)
+
+    # collect all terms to a single string
+    terms = merge_terms(df)
+
+    # find sentences that involve the query term
+    for term in terms:
+        sentences = search_sentences(content, term)
+        # collect meta data info for the query term
+        meta = merge_term_metadata(df, term)
+
+    return term_info
+
+
+def merge_by_chapter(df):
+    '''
+    function that merge all paragraphs in the textbook given the dataframe representation of the textbook
+    :param df: dataframe representation of the textbook
+    :return: a single string of content in the textbook, all lowercase.
+    '''
+    # init the data columns
+    book = []
+    chapter = []
+    chapter_title = []
+    module = []
+    module_title = []
+    module_id = []
+    terms = []
+    p_content = []
+
+    chapters = df['chapter'].unique()
+    for c in chapters:
+        b = df['book'].unique()
+        c_t = df['chapter_title'].unique()
+        modules = df[df['chapter'] == c]['module'].unique()
+        for m in modules:
+            m_t = df['module_title'].unique()
+            m_id = df['module_id'].unique()
+            subset = df.loc[(df['chapter']==c) & (df['module']==m)]
+            # merge content and lowercase it
+            p_c = ' '.join(subset['p_content'])
+            p_c = p_c.lower()
+            # merge terms
+            t = merge_terms(subset)
+            # add to a data lists
+            book.append(b)
+            chapter.append(c)
+            chapter_title.append(c_t)
+            module.append(m)
+            module_title.append(m_t)
+            module_id.append(m_id)
+            p_content.append(p_c)
+            terms.append(t)
+    # merge to dataframe
+    df_copy = pd.DataFrame({'book':book,
+                            'chapter':chapter,
+                            'chapter_title':chapter_title,
+                            'module':module,
+                            'module_title':module_title,
+                            'module_id':module_id,
+                            'terms':terms,
+                            'p_content':p_content})
+    return df_copy
+
+
+def merge_terms(df):
+    '''
+    function to merge all terms
+    :param df: dataframe representation of the textbook
+    :return: an array of array of terms
+    '''
+    terms = []
+    for row in df.index:
+        terms += ast.literal_eval(df['terms'][row])
+    terms = list(set(map(tuple, terms)))
+    return terms
+
+def search_sentences(df, term):
+    '''
+    function to search for all sentences that involve the term
+    :param df:
+    :param term:
+    :return: an array of sentences (strings)
+    '''
