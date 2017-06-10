579dbe2881c8cd14e9b848c405484a83e33771d3
moonlightlane <zw16@rice.edu>
2017-06-09 23:54:14 -0500

    added function to search for embedded terms, query a particular term in the dataframe or in the list of terms, and a possibly wrong revision of search sentences that contain a query term function

--- API/content_helper.py
+++ API/content_helper.py
@@ -38,28 +38,28 @@ def lowercase_term(df):
     return df
 
 
-# def arrayize_term(df):
-#     """
-#     function that turns each row of the term column into array of arrays instead of array of strings. Here,
-#     each array in the array contains just a single string.
-#
-#     Example: "[['term 1'], ['term 2'], ['term 3'], ...]"
-#     (it is a string of array because that is what is the format originally stored in the df)
-#
-#     :param df: the data frame containing all the textbook content
-#     :return: with modified term column as described above
-#     """
-#     terms_arrays = df.terms.unique()
-#
-#     for idx in range(0,len(terms_arrays)):
-#         terms = ast.literal_eval(terms_arrays[idx])
-#
-#         for i in range(0, len(terms)):
-#             terms[i] = [terms[i]]
-#
-#         df['terms'].replace(terms_arrays[idx], str(terms), inplace=True)
-#
-#     return df
+def arrayize_term(df):
+    """
+    function that turns each row of the term column into array of arrays instead of array of strings. Here,
+    each array in the array contains just a single string.
+
+    Example: "[['term 1'], ['term 2'], ['term 3'], ...]"
+    (it is a string of array because that is what is the format originally stored in the df)
+
+    :param df: the data frame containing all the textbook content
+    :return: with modified term column as described above
+    """
+    terms_arrays = df.terms.unique()
+
+    for idx in range(0,len(terms_arrays)):
+        terms = ast.literal_eval(terms_arrays[idx])
+
+        for i in range(0, len(terms)):
+            terms[i] = [terms[i]]
+
+        df['terms'].replace(terms_arrays[idx], str(terms), inplace=True)
+
+    return df
 
 
 def pluralize_term(df):
@@ -202,24 +202,16 @@ def merge_by_chapter(df):
     return df_copy
 
 
-def unique_terms(df):
-    '''
-    find unique terms in the content
-    :param df: original dataframe content
-    :return: a string of lists of lists of unique terms
-    '''
-    return df['terms'].unique()
-
-
 def original_terms(path_to_original_df):
-    workspace_path = '/home/jack/Documents/openstaxTextbook/data/biology/'  # VARIABLE
     # read the content file
-    f = workspace_path + 'content.csv'
+    f = path_to_original_df + 'content.csv'
     df = pd.read_csv(f)
     terms = []
     for row in df.index:
-        terms += ast.literal_eval(df['terms'][row])
-        terms = terms.unique().lower()
+        for term in ast.literal_eval(df['terms'][row]):
+            terms.append(term.lower())
+    terms = list(set(terms))
+        # terms = terms.unique().lower()
     return terms
 
 
@@ -231,7 +223,10 @@ def merge_terms(df):
     '''
     terms = []
     for row in df.index:
-        terms += ast.literal_eval(df['terms'][row])
+        if isinstance(df['terms'][row], str):
+            terms += ast.literal_eval(df['terms'][row])
+        elif isinstance(df['terms'][row], list):
+            terms += df['terms'][row]
     terms = list(set(map(tuple, terms)))
     return terms
 
@@ -333,62 +328,212 @@ def coref_mention_get_info(mention):
            startIndex_, endIndex_, headIndex_, sentNum_, position_, isRep_
 
 
-def search_sentences(content, term, json_data):
+def search_sentences(content, term, json_data, embedded_terms):
     '''
     function to search for all sentences that involve the term
     :param df:
     :param term: this is an array of all possible forms of this term
-    :return: an array of sentences (strings)
+    :return: an array terms (each is a string of list), and an array of dict of sentences
+             corresponding to each term
     '''
-    term_sents = []
-    # search in each sentence in the content
-    sents = json_data['sentences']
-    for sent in sents:
-        # get start and end character index
-        sent_begin_idx = sent['tokens'][0]['characterOffsetBegin']
-        sent_end_idx   = sent['tokens'][-1]['characterOffsetEnd']
-        sent_text = content[sent_begin_idx:sent_end_idx]
-        # search for the term
-        for t in term:
-            search_result = re.findall('\\b'+t+'\\b', sent_text, flags=re.IGNORECASE)
-            if len(search_result) > 0:
-                sent_dict = {'index': sent['index'],
-                             'text' : sent_text}
-                term_sents.append(sent_dict)
-    # search in corefs
-    corefs = json_data['corefs']
-    for key in corefs.keys():
-        ADD = False
-        for item in corefs[key]:
-            # get info from each dict in the coref mention
-            id_, text_, type_, number_, gender_, animacy_, \
-            startIndex_, endIndex_, headIndex_, sentNum_, \
-            position_, isRep_ = coref_mention_get_info(item)
-            # do a string match of the representative mention and the term
-            if isRep_:
+    # the input term and each entry in embedded terms should all be tuples
+    if type(term) != type(ast.literal_eval(embedded_terms[embedded_terms.keys()[0]][0])):
+        print('input term and embedded terms type does not match')
+        return 0
+    else:
+        # check if the query term is embedded in another term
+        if str(term) in embedded_terms.keys():
+            EMBEDDED = True
+            embedded_term = embedded_terms[str(term)]
+        else:
+            EMBEDDED = False
+        term_sents = []
+        # search in each sentence in the content
+        sents = json_data['sentences']
+        for sent in sents:
+            APPEND = True
+            # get start and end character index of the sentence
+            # in order to slice a single sentence from the content
+            sent_begin_idx = sent['tokens'][0]['characterOffsetBegin']
+            sent_end_idx   = sent['tokens'][-1]['characterOffsetEnd']
+            sent_text = content[sent_begin_idx:sent_end_idx]
+            sent_dict = {'index': sent['index'], 'text': sent_text}
+            # if the term is embedded in another term, want to search for the term that embeds
+            # the query term. If the sentence does not contain the embedded term, then
+            # append the sentence to dict
+            if EMBEDDED:
+                for e_t in embedded_term:
+                    for e in e_t:
+                        search_result = re.findall('\\b' + e + '\\b', sent_text, flags=re.IGNORECASE)
+                        # change the append flag only if the embedded term does not appear in sent
+                        if search_result > 0:
+                            APPEND = False
+                        # else, attach
+            # search for the query term in the sentence text
+            if APPEND:
                 for t in term:
-                    if t == text_:
-                        ADD = True
-            if not isRep_:
-                sent = sents[sentNum_ - 1]
-                sent_begin_idx = sent['tokens'][0]['characterOffsetBegin']
-                sent_end_idx = sent['tokens'][-1]['characterOffsetEnd']
-                sent_text = content[sent_begin_idx:sent_end_idx]
-                sent_dict = {'index': sent['index'],
-                             'text': sent_text}
-                if ADD:
-                    if len(term_sents) == 0:
+                    search_result = re.findall('\\b'+t+'\\b', sent_text, flags=re.IGNORECASE)
+                    if len(search_result) > 0:
                         term_sents.append(sent_dict)
-                    else:
-                        for existing_sent in term_sents:
-                            if existing_sent['index'] == sentNum_-1:
-                                ADD = False
-                        if ADD:
+        # search in corefs
+        corefs = json_data['corefs']
+        for key in corefs.keys():
+            APPEND = True
+            for item in corefs[key]:
+                # get info from each dict in the coref mention
+                id_, text_, type_, number_, gender_, animacy_, \
+                startIndex_, endIndex_, headIndex_, sentNum_, \
+                position_, isRep_ = coref_mention_get_info(item)
+                # do a string match of the representative mention and the term
+                if isRep_:
+                    # check if the embedded term is contained in the coref text
+                    if EMBEDDED:
+                        for e_t in embedded_term:
+                            for e in e_t:
+                                search_result = re.findall('\\b' + e + '\\b', text_, flags=re.IGNORECASE)
+                                # change the append flag only if the embedded term does not appear in sent
+                                if search_result > 0:
+                                    APPEND = False
+                    if APPEND:
+                        for t in term:
+                            search_result = re.findall('\\b' + t + '\\b', text_, flags=re.IGNORECASE)
+                            if search_result > 0:
+                                ADD = True
+                if not isRep_:
+                    sent = sents[sentNum_ - 1]
+                    sent_begin_idx = sent['tokens'][0]['characterOffsetBegin']
+                    sent_end_idx = sent['tokens'][-1]['characterOffsetEnd']
+                    sent_text = content[sent_begin_idx:sent_end_idx]
+                    sent_dict = {'index': sent['index'],
+                                 'text': sent_text}
+                    if ADD:
+                        if len(term_sents) == 0:
                             term_sents.append(sent_dict)
-    return term_sents
+                        else:
+                            for existing_sent in term_sents:
+                                if existing_sent['index'] == sentNum_-1:
+                                    ADD = False
+                            if ADD:
+                                term_sents.append(sent_dict)
+        return term_sents
+
+
+def query_term_in_df(df, query):
+    '''
+    check if a particular term exist in the df
+    :param terms:
+    :param query:
+    :return:
+    '''
+    HAS_TERM = False
+    chapter = []
+    module_id = []
+    for row in df.index:
+        terms = df['terms'][row]
+        if isinstance(terms, str):
+            terms = ast.literal_eval(terms)
+        for term in terms:
+            if isinstance(term, tuple):
+                for item in term:
+                    if query.lower() in item.lower():
+                        HAS_TERM = True
+                        if df['chapter'][row] not in chapter:
+                            chapter.append(df['chapter'][row])
+                        if df['module_id'][row] not in module_id:
+                            module_id.append(df['module_id'][row])
+            elif isinstance(term, str):
+                if query.lower() in term.lower():
+                    HAS_TERM = True
+                    if df['chapter'][row] not in chapter:
+                        chapter.append(df['chapter'][row])
+                    if df['module_id'][row] not in module_id:
+                        module_id.append(df['module_id'][row])
+    return HAS_TERM, chapter, module_id
+
+
+def query_term_in_term_array(term_array, query):
+    '''
+    query a particular term (in the form of a string) in the term array
+    (can either be a string of list or a list)
+    :param term_array:
+    :param query:
+    :return:
+    '''
+    HAS_TERM = False
+    PRINT = True
+    if isinstance(term_array, str):
+        print('string format term array input detected.')
+        term_array = ast.literal_eval(term_array)
+    for term in term_array:
+        if isinstance(term, str):
+            if PRINT:
+                print('string format term detected.')
+                PRINT = False
+            if query.lower() in term.lower():
+                HAS_TERM = True
+        elif isinstance(term, tuple) or isinstance(term, list):
+            if PRINT:
+                print('list or tuple term format detected.')
+                PRINT = False
+            for item in term:
+                if query.lower() in item.lower():
+                    HAS_TERM = True
+    return HAS_TERM
 
 
 # TODO: add function to check whether the single word is surrounded by other noun phrases
+def find_embedded_terms(terms):
+    '''
+    function to loop through the list of term arrays (each of the array contains
+    variants of the particular term) and output a dict of the following format:
+    {
+        '["term v1","term v2", "term v3"]': [[enclosed term array 1], [enclosed term array 2]]
+
+    }
+    For example: since 'cell' is embedded in 'cell wall' and 'cell structure'
+                 the following entry should be produced:
+    {
+        '["cell", "cells"]': [['cell wall', 'cell walls'], ['cell structure', 'cell structures']]
+    }
+    :param terms: a list of array of terms
+    :return:
+    '''
+    #TODO this is a very ugly and slow implementation
+    enclosed_terms = {}
+    for query_term_array in terms:
+        enclosed_temp = []
+        for candidate_term_arrary in terms:
+            # init a flag indicating whether the candidate term array contains any variants
+            # in the query term array
+            IS_ENCLOSED = False
+            # here I made the reasonable assumption that if a term is to be enclosed in another
+            # term, the length of the query term must be less than the candidate term
+            # Example: length of "cell" is obviously less than "cell wall"
+            if query_term_array != candidate_term_arrary and \
+               len(min(query_term_array)) < len(min(candidate_term_arrary)):
+                for query_term in query_term_array:
+                    for candidate_term in candidate_term_arrary:
+                        #TODO an ad-hoc way to remove parenthesis
+                        #TODO there are terms such as "revolution theory (darvian theory)"
+                        #TODO that needs a more principled way to extract things inside parenthesis
+                        query_term = re.sub('[()]', '', query_term)
+                        try:
+                            search_result = re.findall('\\b' + query_term + '\\b',
+                                                   candidate_term, flags=re.IGNORECASE)
+                        except:
+                            print(query_term)
+                            print(candidate_term)
+
+                        if len(search_result) > 0:
+                            IS_ENCLOSED = True
+                if IS_ENCLOSED:
+                    enclosed_temp.append(candidate_term_arrary)
+                    enclosed_terms[str(query_term_array)] = enclosed_temp
+                    break
+
+    return enclosed_terms
+
+
 # TODO: more robust search function in corefs that resolves pronoun, article words
 #       for example, "the results" in coref should match with "results"
 
